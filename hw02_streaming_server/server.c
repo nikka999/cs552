b1d5ad38a7af8ca859736b59e2b6bf2bf6dd9950
Mike Weng <mikeweng@bu.edu>
2012-11-25 16:43:43 -0500

    correct stupid mistake

--- hw02_streaming_server/server.c
+++ hw02_streaming_server/server.c
@@ -191,12 +191,12 @@ void *dispatcher(void *thread_id){
             write(wm[i].fd, &len, sizeof(size_t));
             
             // Send size
-            len = sizeof(cols*rows*3);
+            len = (cols*rows*3);
             len = htonl(len);
             write(wm[i].fd, &len, sizeof(size_t));
             
             // Send image
-            write(wm[i].fd, &buf, sizeof(cols*rows*3));
+            write(wm[i].fd, &buf, cols*rows*3);
             
 			//write(wm[i].fd, msg, strlen(msg));
 		}
