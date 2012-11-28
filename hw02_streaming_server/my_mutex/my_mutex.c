72a7cedecad7af2a5dc6db2e377a9c33dbf9ac32
Nikka Ghalili <nikka999@gmail.com>
2012-11-28 18:49:15 -0500

    alert

--- hw02_streaming_server/my_mutex/my_mutex.c
+++ hw02_streaming_server/my_mutex/my_mutex.c
@@ -1,14 +1,19 @@
 #include <linux/module.h>	
 #include <linux/kernel.h>	
+#include <linux/init.h>	
 
-int init_module(void)
+static int __init init_mutex(void)
 {
 	printk(KERN_ALERT "Hello world 1.\n");
 
 	return 0;
 }
 
-void cleanup_module(void)
+static void __exit mutex_cleanup(void)
 {
 	printk(KERN_ALERT "Goodbye world 1.\n");
 }
+
+
+module_init(init_mutex);
+module_exit(mutex_cleanup);
