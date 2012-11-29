7ada6219996ccacf21b1325f21e09b0e7b28079d
Nikka Ghalili <nikka999@gmail.com>
2012-11-28 19:27:36 -0500

    mutex calls

---
+++ hw02_streaming_server/my_mutex/mutex_calls.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+
+#define IOCTL_TEST _IOW(0, 6, struct ioctl_test_t)
+
+int main () {
+
+  /* attribute structures */
+  struct ioctl_test_t {
+    int field1;
+    char field2;
+  } ioctl_test;
+
+  int fd = open ("/proc/my_mutex", O_RDONLY);
+
+  ioctl_test.field1 = 10;
+  ioctl_test.field2 = 'a';
+
+  ioctl (fd, IOCTL_TEST, &ioctl_test);
+
+  return 0;
+}
