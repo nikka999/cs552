581fb8896f8258b5ca6612b77dff8a8838ec9d6e
Nikka Ghalili <nikka999@gmail.com>
2012-11-28 19:17:37 -0500

    adding ioctl stuff

--- hw02_streaming_server/my_mutex/my_mutex.c
+++ hw02_streaming_server/my_mutex/my_mutex.c
@@ -9,16 +9,38 @@
 
 void my_printk(char *string);
 
+struct ioctl_test_t {
+  int field1;
+  char field2;
+};
+
+#define IOCTL_TEST _IOW(0, 6, struct ioctl_test_t)
+
+static int pseudo_device_ioctl(struct inode *inode, struct file *file,
+                               unsigned int cmd, unsigned long arg);
+
+static struct file_operations pseudo_dev_proc_operations;
+
+static struct proc_dir_entry *proc_entry;
+
 static int __init init_mutex(void)
 {
-	my_printk("Hello world 1.\n");
-
+	my_printk("Loading my_mutex module\n");
+	pseudo_dev_proc_operations.ioctl = pseudo_device_ioctl;
+	
+	proc_entry = create_proc_entry("ioctl_test", 0444, NULL);
+	if(!proc_entry)
+  	{
+    	my_printk("Error creating /proc entry.\n");
+    	return 1;
+  	}
+  	proc_entry->proc_fops = &pseudo_dev_proc_operations;
 	return 0;
 }
 
 static void __exit mutex_cleanup(void)
 {
-	my_printk("Goodbye world 1.\n");
+	my_printk("Removing my_mutex module\n");
 }
 
 void my_printk(char *string)
@@ -33,5 +55,28 @@ void my_printk(char *string)
   }
 }
 
+static int pseudo_device_ioctl(struct inode *inode, struct file *file,
+                                unsigned int cmd, unsigned long arg)
+{
+	struct ioctl_test_t ioc;
+
+  	switch (cmd){
+
+  		case IOCTL_TEST:
+    		copy_from_user(&ioc, (struct ioctl_test_t *)arg,
+				sizeof(struct ioctl_test_t));
+    		printk("<1> ioctl: call to IOCTL_TEST (%d,%c)!\n",
+           		ioc.field1, ioc.field2);
+
+    		my_printk ("Got msg in kernel\n");
+    		break;
+
+  		default:
+    		return -EINVAL;
+    		break;
+  	}
+
+  	return 0;
+}
 module_init(init_mutex);
 module_exit(mutex_cleanup);
