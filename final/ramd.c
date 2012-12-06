406d16a37a018776058d7e22812165d869fe4488
Mike Weng <mikeweng@bu.edu>
2012-12-06 16:14:40 -0500

    First commit w/ files for final project

---
+++ final/ramd.c
@@ -0,0 +1,49 @@
+#include <linux/vmalloc.h>
+#include <linux/init.h>
+#include <linux/errno.h>
+#include <linux/module.h>
+#include <linux/proc_fs.h>
+
+MODULE_LICENSE("GPL");
+
+static char *ramdisk;
+static struct proc_dir_entry *proc_entry;
+static int ramdisk_ioctl();
+
+static int __init init_routine(void) {
+	printk("<1> Loading RAMDISK Module\n");
+
+	// Create /proc entry first. 666 for Read/Write
+	proc_entry = create_proc_entry("ramdisk", 0666, NULL);
+	if (!proc_entry) {
+		printk("<1> Error creating /proc entry.\n");
+		return 1;
+	}
+	proc_entry->proc_fops = &ramdisk_ioctl;
+
+	// vmalloc for 2MB
+	ramdisk = (char *)vmalloc(2097150);
+
+	return 0;
+}
+
+static void __exit exit_routine(void) {
+	printk("<1> Exiting RAMDISK Module\n");
+	// Free ramdisk
+	vfree(ramdisk);
+
+	// Remove /proc entry
+	remove_proc_entry("ramdisk", NULL);
+	return;
+}
+
+/** 
+* Ramdisk entry point
+*/
+
+static int ramdisk_ioctl() {
+return 0;
+}
+
+module_init(init_routine);
+module_exit(exit_routine);
