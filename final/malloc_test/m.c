03b18b0f67bbbf9863334d7d86f9336aa21c2a58
Mike Weng <mikeweng@bu.edu>
2012-12-10 17:27:42 -0500

    alloc file cont

--- final/malloc_test/m.c
+++ final/malloc_test/m.c
@@ -8,7 +8,9 @@
 #define MAX_FILE_COUNT INODE_NUMBER
 #define MAX_FILE_SIZE ((64*256)+(64*64*256)+256*8)
 
-
+// For fd. Since the maximum file number is 1024. We assign it to be 1024.
+// Put fd number to the fd_table.
+short fd_table[1024];
 struct Ramdisk *rd;
 
 void init_fs() {
@@ -31,13 +33,46 @@ void init_fs() {
     // EOF init
 }
 
+int set_inode_reg_file(unsigned char *file, long int size) {
+    //printf("Size=?%ld, Max_size?=%d\n", size, MAX_FILE_SIZE);
+    if (size > MAX_FILE_SIZE) {
+        return -1;
+    }
+    return 0;
+    // 1st Block: 0~255; 2nd Block: 256~511; 3rd Block: 512~767; 4th Block: 768~1023; 5th Block: 1024~1279; 6th Block: 1280~1535; 7th Block: 1536~1791; 8th Block: 1792~2047; TOTAL:2048
+    int number_blocks = size % 256;
+    if (number_blocks > SHOW_FREEBLOCK_COUNT) {
+        return -1;
+    }
+    int i = 1;
+    while (i <= number_blocks) {
+        if (i<=8) {
+            // Direct Block Regular File.
+            // 1. Find a free block;
+            // 2. Assign block to inode index X-1, and write to block
+            // 3. Decrement freeblock count;
+            DECR_FREEBLOCK;
+        } else if (i > 8 && i <= 72) {
+            // Single Redirection block, are block #9~72
+        } else if (i > 72 && i <= 4168) {
+            // Double Redirection block, are block #73~4168
+        }
+        i++;
+    }
+    
+}
 
 int main() {
     init_fs();
+    
+    unsigned char file[ MAX_FILE_SIZE +1];
+    int x = set_inode_reg_file(file, sizeof(file));
+    printf("Size=?%d, Allocated?=%d\n", (int)sizeof(file), x);
+    
 #ifdef debug
     PRINT_FREEINODE_COUNT;
     PRINT_FREEBLOCK_COUNT;
-#endif
+
     // test set filename
     char n[16] = "non-root";
     SET_DIR_ENTRY_NAME(0, 0, n);
@@ -80,11 +115,11 @@ int main() {
     PRINT_BITMAP_BIT_STATUS(block_number);
     
     PRINT_BITMAP_BYTE(byte);
-    
+#endif    
 
     // Test freeing a block. directly
-    memset(rd->pb[])
     
+
 
 }
 
