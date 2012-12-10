e7f0df8f9957d246713e9549bdc930073c49b035
Mike Weng <mikeweng@bu.edu>
2012-12-10 15:13:01 -0500

    push

--- final/malloc_test/m.c
+++ final/malloc_test/m.c
@@ -4,47 +4,11 @@
 
 #include "m.h"
 
-/** General methods */
-#define GET_BYTE(x) (ramdisk[x])
-// Get bit from left to right. i.e. 01234567
-#define GET_BIT(x, y) (((GET_BYTE(x)) >> (7-y)) & 0x01)
-// Create 1 on bit y (all others 0)
-#define BIT_MASK_1(y) ((0x01 << (7-y)))
-// Create 0 on bit y (all others 1)
-#define BIT_MASK_0(y) (~BIT_MASK_1(y))
-// Get integer pointer
-#define GET_INT_PTR(X) ((int *)&ramdisk[X])
-// Get integer
-#define GET_INT(X) (*(int *)&ramdisk[X])
-// Set Integer
-#define SET_INT(X, Y); {int* temp=GET_INT_PTR(X); *temp=Y;}
-
-/** Bitmap block */ 
-#define BITMAPBLOCK_START (INODEBLOCK_END + 1)
-#define BITMAPBLOCK_SIZE (BLOCK_SIZE * 4)
-#define BITMAPBLOCK_END (BITMAPBLOCK_START + BITMAPBLOCK_SIZE - 1)
-/** Bitmap helper methods*/
-/** Total blocks possible is 2MB/256 = 8192. Subtract supepr/inode/bitmap (1+256+4) => 7931 blocks. In which 7931 is the total blocks for our partition. Also, 8192 happens to be the total bit length of our bitmap. Hence, our bitmap can use MOD operator to easily manipulate bits with the starting address of the block. (We ignore first 261 bits since that is reserved for our super+inode+bitmap blocks)
- */
-// Given a location(block starting address) find the block number, i.e. location of the bit in bitmap
-#define GET_BLOCK_NUMBER(X) (X / 256)
-#define GET_BYTE_NUMBER(X) ((GET_BLOCK_NUMBER(X) / 8) + BITMAPBLOCK_START)
-#define GET_BYTE_OFFSET(X) (GET_BLOCK_NUMBER(X) % 8)
-#define SET_BIT_TO_1_BYTE(x, y) (GET_BYTE(x) = ((GET_BYTE(x)) | BIT_MASK_1(y)))
-#define SET_BIT_TO_0_BYTE(x, y) (GET_BYTE(x) = ((GET_BYTE(x)) & BIT_MASK_0(y)))
-
-/** Bitmap block methods */
-#define SET_BITMAP_ALLOCATE_BLOCK(X) (SET_BIT_TO_1_BYTE((GET_BYTE_NUMBER(X)),(GET_BYTE_OFFSET(X))))
-#define SET_BITMAP_FREE_BLOCK(X) (SET_BIT_TO_0_BYTE((GET_BYTE_NUMBER(X)),(GET_BYTE_OFFSET(X))))
-#define SEE_BITMAP_ALLOCATE(X) ((GET_BIT(GET_BYTE_NUMBER(X), GET_BYTE_OFFSET(X)) == 1) ? 1 : 0)
-#define SEE_BITMAP_FREE(X) ((GET_BIT(GET_BYTE_NUMBER(X), GET_BYTE_OFFSET(X)) == 0) ? 1 : 0)
-
 /** MAXIMUM */
 #define MAX_FILE_COUNT INODE_NUMBER
 #define MAX_FILE_SIZE ((64*256)+(64*64*256)+256*8)
 
-char dir[4] = "dir";
-char reg[4] = "reg";
+
 struct Ramdisk *rd;
 
 void init_fs() {
@@ -68,32 +32,59 @@ void init_fs() {
 }
 
 
-// Write to file block/and get from. (NO REDIRECTION)
-#define WRITE_TO_FILE(BLOCK, BYTES); {memcpy(rd->pb[BLOCK].reg.byte, BYTES, 256);}
-#define GET_FROM_FILE(BLOCK) rd->pb[BLOCK].reg.byte
-
-// Assign BLOCK location to #Inode's #index. => Assign location in inode (regular file block (0~8))
-#define ASSIGN_LOCATION(INODE, INDEX, BLOCK); {rd->ib[INODE].blocks[INDEX] = &rd->pb[BLOCK];}
-#define GET_FROM_LOCATION(INODE, INDEX) (*rd->ib[INODE].blocks[INDEX]).reg.byte
-
 int main() {
     init_fs();
 #ifdef debug
     PRINT_FREEINODE_COUNT;
     PRINT_FREEBLOCK_COUNT;
+#endif
     // test set filename
     char n[16] = "non-root";
     SET_DIR_ENTRY_NAME(0, 0, n);
     PRINT_DIR_ENTRY_NAME(0, 0);
-#endif
-    // test write to reg file block
-    unsigned char x[260] = "asdfasdfasdfasdfasdfasdf";
-    WRITE_TO_FILE(1, x)
-    printf("Print: %s\n", GET_FROM_FILE(1));
 
-    // Assign location in inode (regular file block (0~8))
+    // test write to reg file block
+    unsigned char x[260] = "testing=testing2=testing3";
+    WRITE_TO_FILE(1, x)
+    printf("Print direct block write: %s\n", GET_FROM_FILE(1));
+
+    // Assign location in inode (regular file block (0~7))
     ASSIGN_LOCATION(1, 0, 1);
-    printf("Print: %s\n", GET_FROM_LOCATION(1, 0));
+    printf("Print direct block Inode: %s\n", GET_FROM_LOCATION(1, 0));
     
+    // Single redirection
+    ASSIGN_LOCATION(1, 8, 2);
+    ASSIGN_LOCATION_SINGLE_RED(1, 0, 3);
+    WRITE_TO_FILE(3, x);
+    printf("Print single redir block: %s\n", GET_FROM_LOCATION_SINGLE_RED(1, 0));
+    
+    // Double redirection
+    ASSIGN_LOCATION(1, 9, 4);
+    ASSIGN_LOCATION_DOUBLE_FST_RED(1, 0, 5);
+    WRITE_TO_FILE(6, x);
+    ASSIGN_LOCATION_DOUBLE_SND_RED(1, 0, 0, 6);
+    printf("Print double redir block: %s\n", GET_FROM_LOCATION_DOUBLE_RED(1, 0, 0));
+
+    
+    // Test bitmap
+    int byte=0;
+    int block_number =7;
+    SET_BITMAP_ALLOCATE_BLOCK(block_number);
+    
+    PRINT_BITMAP_BIT_STATUS(block_number);
+    
+    PRINT_BITMAP_BYTE(byte);
+    
+    SET_BITMAP_FREE_BLOCK(block_number);
+    
+    PRINT_BITMAP_BIT_STATUS(block_number);
+    
+    PRINT_BITMAP_BYTE(byte);
+    
+
+    // Test freeing a block. directly
+    memset(rd->pb[])
+    
+
 }
 
--- final/malloc_test/m.h
+++ final/malloc_test/m.h
@@ -18,14 +18,55 @@
 #define INODEBLOCK_SIZE (BLOCK_SIZE * INODEBLOCK_NUMBER)
 #define INODE_SIZE 64
 #define INODE_NUMBER (INODEBLOCK_SIZE/INODE_SIZE)
-
+char dir[4] = "dir";
+char reg[4] = "reg";
 #define SET_INODE_TYPE_DIR(INDEX); {memcpy(rd->ib[INDEX].type, dir, 4);}
 #define SET_INODE_TYPE_REG(INDEX); {memcpy(rd->ib[INDEX].type, reg, 4);}
 #define SET_INODE_SIZE(INDEX, SIZE); {rd->ib[INDEX].size = SIZE;}
 
+
+/** General methods */
+// Create 1 on bit y (all others 0)
+#define BIT_MASK_1(y) ((0x01 << (7-y)))
+// Create 0 on bit y (all others 1)
+#define BIT_MASK_0(y) (~BIT_MASK_1(y))
+
+/** Bitmap block */
+/** Total blocks possible is 2MB/256 = 8192. Subtract supepr/inode/bitmap (1+256+4) => 7931 blocks. In which 7931 is the total blocks for our partition. Since we are only interested in whether a partition block is free or not. We can use the block number indicate the bit in the bitmap. I.e. Get which byte the location is in, and the offset. Then set it to 1.
+ */
+#define GET_BYTE_NUMBER(BLOCK) (BLOCK / 8)
+#define GET_BYTE_OFFSET(BLOCK) (BLOCK % 8)
+#define SET_BITMAP_ALLOCATE_BLOCK(BLOCK) (rd->bb.byte[GET_BYTE_NUMBER(BLOCK)] = (rd->bb.byte[GET_BYTE_NUMBER(BLOCK)] | BIT_MASK_1(GET_BYTE_OFFSET(BLOCK))))
+#define SET_BITMAP_FREE_BLOCK(BLOCK) (rd->bb.byte[GET_BYTE_NUMBER(BLOCK)] = (rd->bb.byte[GET_BYTE_NUMBER(BLOCK)] & BIT_MASK_0(GET_BYTE_OFFSET(BLOCK))))
+#define PRINT_BITMAP_BYTE(BYTE); {int zzzzzz; for(zzzzzz=0; zzzzzz < 8; zzzzzz++) {printf("%d", (rd->bb.byte[BYTE] >> (7-zzzzzz)) & 0x01);} printf("\n");}
+
+#define PRINT_BITMAP_BIT_STATUS(block_number); {printf("Block %d: allocated?=%d, freed?=%d\n", block_number, SEE_BITMAP_ALLOCATE(block_number), SEE_BITMAP_FREE(block_number));}
+#define SEE_BITMAP_ALLOCATE(BLOCK) (((rd->bb.byte[GET_BYTE_NUMBER(BLOCK)] >> (7-GET_BYTE_OFFSET(BLOCK))) == 1) ? 1 : 0)
+#define SEE_BITMAP_FREE(BLOCK) (((rd->bb.byte[GET_BYTE_NUMBER(BLOCK)] >> (7-GET_BYTE_OFFSET(BLOCK))) == 0) ? 1 : 0)
+
+
 /** Partition */
 #define PARTITION_SIZE (RAMDISK_SIZE-(256*(1+256+4)))
 #define PARTITION_NUMBER (PARTITION_SIZE/BLOCK_SIZE)
+// Partition Methods
+// Write to file block/and get from. (NO REDIRECTION)
+#define WRITE_TO_FILE(BLOCK, BYTES); {memcpy(rd->pb[BLOCK].reg.byte, BYTES, 256);}
+#define GET_FROM_FILE(BLOCK) rd->pb[BLOCK].reg.byte
+
+// Assign BLOCK location to #Inode's #index. => Assign location in inode (regular file block (0~8))
+#define ASSIGN_LOCATION(INODE, INDEX, BLOCK); {rd->ib[INODE].blocks[INDEX] = &rd->pb[BLOCK];}
+#define GET_FROM_LOCATION(INODE, INDEX) (*rd->ib[INODE].blocks[INDEX]).reg.byte
+
+// To assign Single redirection block to #Inode's 8 location. Use ASSIGN_LOCATION(INODE, 8, BLOCK). Use WRITE_TO_FILE(block, bytes) to directly write to block.
+// Assign BLOCK to single redirection block
+#define ASSIGN_LOCATION_SINGLE_RED(INODE, PTR_ENTRY, BLOCK); {rd->ib[INODE].blocks[8]->ptr.blocks[PTR_ENTRY] = &rd->pb[BLOCK];}
+#define GET_FROM_LOCATION_SINGLE_RED(INODE, PTR_ENTRY) (*(*rd->ib[INODE].blocks[8]).ptr.blocks[PTR_ENTRY]).reg.byte
+
+// Double redirection block: Use ASSIGN_LOCATION(Inode, 9, block) for first redirection block. Use WRITE_TO_FILE(block, bytes) to directly write to final block.
+#define ASSIGN_LOCATION_DOUBLE_FST_RED(INODE, PTR_ENTRY, BLOCK); {rd->ib[INODE].blocks[9]->ptr.blocks[PTR_ENTRY] = &rd->pb[BLOCK];}
+#define ASSIGN_LOCATION_DOUBLE_SND_RED(INODE, PTR_ENTRY1, PTR_ENTRY2, BLOCK) {rd->ib[INODE].blocks[9]->ptr.blocks[PTR_ENTRY1]->ptr.blocks[PTR_ENTRY2] = &rd->pb[BLOCK];}
+#define GET_FROM_LOCATION_DOUBLE_RED(INODE, PTR_ENTRY1, PTR_ENTRY2) (*(*(*rd->ib[INODE].blocks[9]).ptr.blocks[PTR_ENTRY1]).ptr.blocks[PTR_ENTRY2]).reg.byte
+
 
 // Copy only 13, leave 14th for Null.
 #define SET_DIR_ENTRY_NAME(BLOCK, ENTRY, NAME); {memcpy(rd->pb[BLOCK].dir.ent[ENTRY].filename, NAME, 13);}
